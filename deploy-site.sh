5f7a4062a078d7d998ae0c0d70e90b3552d1f2d5
Tamas Molnar <stiron@gmail.com>
2016-02-20 15:09:49 +0100

    Upgraded a bit the deployment script

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,15 +1,18 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
-## Install Git and Puppet
+# Install Git and Puppet
 wget -O /tmp/puppetlabs.deb http://apt.puppetlabs.com/puppetlabs-release-`lsb_release -cs`.deb
 dpkg -i /tmp/puppetlabs.deb
-apt-get update
-apt-get -y install git-core puppet
+apt-get -q -y update
+apt-get -q -y install git-core puppet
 
-# Clone the 'puppet' repo
-cd /etc
-mv puppet/ puppet-bak
-git clone https://github.com/stiron/puppet-ububook.git /etc/puppet
+if [[ -d /etc/puppet ]]; then
+  # Backup the puppet directory if exists
+  mv puppet/ puppet-backup
+else
+  # Clone the 'puppet-ububook' repo
+  git clone https://github.com/stiron/puppet-ububook.git /etc/puppet
 
-# Run Puppet initially to set up the auto-deploy mechanism
-puppet apply /etc/puppet/manifests/site.pp
+  # Run Puppet initially to set up the auto-deploy mechanism
+  puppet apply /etc/puppet/manifests/site.pp
+fi
