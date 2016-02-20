71d1926a593649737f8c18e9b76453be935994dc
Tamas Molnar <stiron@gmail.com>
2016-02-20 15:43:03 +0100

    Fixing the virtualbox module's unless statement

--- modules/virtualbox/manifests/init.pp
+++ modules/virtualbox/manifests/init.pp
@@ -2,12 +2,12 @@ class virtualbox {
 
   exec { "wget-virtualbox-deb":
     command => "/usr/bin/wget -O /tmp/virtualbox.deb $vbox_url",
-    unless => "/usr/bin/dpkg -l virtualbox*",
+    unless => [ "test -d /etc/virtualbox", ],
   }
 
   exec { "install-virtualbox-deb":
     command => "/usr/bin/dpkg -i /tmp/virtualbox.deb",
-    unless => "/usr/bin/dpkg -l virtualbox*",
+    unless => [ "test -d /etc/virtualbox", ],
   }
 
 }
