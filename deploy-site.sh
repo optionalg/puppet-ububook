d34c5f59e2555db958a3a21ca332291e3e2fd3a4
Tamas Molnar <stiron@gmail.com>
2016-02-25 17:02:02 +0100

    Reverting to a working version

--- deploy-site.sh
+++ deploy-site.sh
@@ -6,13 +6,11 @@ dpkg -i /tmp/puppetlabs.deb
 apt-get -q -y update
 apt-get -q -y install git-core puppet
 
-if [[ -d /etc/puppet ]]; then
-  # Backup the puppet directory if exists
-  mv puppet/ puppet-backup
-else
-  # Clone the 'puppet-ububook' repo
-  git clone https://github.com/stiron/puppet-ububook.git /etc/puppet
+# Backup the puppet directory if exists
+mv puppet/ puppet-backup
 
-  # Run Puppet initially to set up the auto-deploy mechanism
-  puppet apply /etc/puppet/manifests/site.pp
-fi
+# Clone the 'puppet-ububook' repo
+git clone https://github.com/stiron/puppet-ububook.git /etc/puppet
+
+# Run Puppet initially to set up the auto-deploy mechanism
+puppet apply /etc/puppet/manifests/site.pp
