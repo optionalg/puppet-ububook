fb36c552838980f420385b53e5bca2d5cc47b084
Tamas Molnar <tamas.t.molnar@ericsson.com>
2016-03-04 13:35:05 +0100

    Fixed some problems again in the site-deployer

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ -d /etc/puppet ]]; then
-  cd /etc/puppet
-  git pull
-else
-  wget -O /tmp/puppetlabs.deb http://apt.puppetlabs.com/puppetlabs-release-`lsb_release -cs`.deb
-  dpkg -i /tmp/puppetlabs.deb
-  apt-get -q -y update
-  apt-get -q -y install git-core puppet
-  git clone https://github.com/stiron/puppet-ububook.git /etc/puppet
-fi
+# Installing Puppet on the system
+wget -O /tmp/puppetlabs.deb http://apt.puppetlabs.com/puppetlabs-release-`lsb_release -cs`.deb
+dpkg -i /tmp/puppetlabs.deb
+apt-get -q -y update
+apt-get -q -y install git-core puppet
+
+# Moving the default Puppet directory from /etc
+mv /etc/puppet /etc/puppet-backup
+
+# Cloning the ububook repo to /etc/puppet
+git clone https://github.com/stiron/puppet-ububook.git /etc/puppet
