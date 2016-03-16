5fc713a20e29c30883e8c75db0c5d1de1a9a32d2
Tamas Molnar <stiron@gmail.com>
2016-03-16 19:29:03 +0100

    Added date to backup file

--- deploy-site.sh
+++ deploy-site.sh
@@ -6,8 +6,8 @@ dpkg -i /tmp/puppetlabs.deb
 apt-get -q -y update
 apt-get -q -y install git-core puppet
 
-# Moving the default Puppet directory from /etc
-mv /etc/puppet /etc/puppet-backup
+# Moving the old Puppet directory to a backup
+mv /etc/puppet /etc/puppet-`date +%Y-%m-%d`
 
 # Cloning the ububook repo to /etc/puppet
 git clone https://github.com/stiron/puppet-ububook.git /etc/puppet
