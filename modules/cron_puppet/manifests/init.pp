7fd654264dca4720ca6fad4c7ce8bcaca4a7aef1
Tamas Molnar <stiron@gmail.com>
2016-03-31 19:50:37 +0200

    Strings have been quoted

--- modules/cron_puppet/manifests/init.pp
+++ modules/cron_puppet/manifests/init.pp
@@ -4,35 +4,35 @@ class cron_puppet {
         path   => '/etc/puppet/.git/hooks/post-merge',
         source => 'puppet:///modules/cron_puppet/post-merge',
         mode   => '0755',
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
     }
     file { 'puppet-logrotate':
         ensure => file,
         path   => '/etc/logrotate.d/puppet',
         source => 'puppet:///modules/cron_puppet/puppet.logrotate',
         mode   => '0644',
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
     }
     cron { 'git-pull':
         ensure  => present,
         command => 'cd /etc/puppet ; /usr/bin/git pull',
-        user    => root,
+        user    => 'root',
         minute  => '*/20',
         require => File['post-hook'],
     }
     cron { 'puppet-apply':
         ensure  => present,
         command => '/etc/puppet/.git/hooks/post-merge',
-        user    => root,
+        user    => 'root',
         minute  => '55',
         require => File['post-hook'],
     }
     cron { 'puppet-apply-reboot':
         ensure  => present,
         command => '/etc/puppet/.git/hooks/post-merge',
-        user    => root,
+        user    => 'root',
         special => 'reboot',
         require => File['post-hook'],
     }
