05c408632a262ce7d249b6b526d85c7e508870c5
Tamas Molnar <stiron@gmail.com>
2016-04-30 07:10:55 +0200

    Updated rspec

--- modules/tmux/spec/classes/tmux_spec.rb
+++ modules/tmux/spec/classes/tmux_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
 describe 'tmux' do
+  let(:local_user) {'tmolnar'}
+  
   it { is_expected.to compile.with_all_deps }
   it { is_expected.to contain_package('tmux-package').with_ensure('present') }
-  it { is_expected.to contain_file('tmux-config') }
+  it { is_expected.to contain_file('tmux-config').with(
+    'ensure' => 'file',
+    'path'   => '/home/tmolnar/.tmux.conf',
+    'source' => 'puppet:///modules/tmux/.tmux.conf',
+    'mode'   => '0755',
+    'owner'  => 'tmolnar',
+    'group'  => 'tmolnar'
+  ) }
 end
