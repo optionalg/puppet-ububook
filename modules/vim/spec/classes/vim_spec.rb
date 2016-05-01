494731601b35a5681d520d5d8dd3d7bd6833c4b2
Tamas Molnar <stiron@gmail.com>
2016-05-01 07:53:30 +0200

    Extended the rspec test

--- modules/vim/spec/classes/vim_spec.rb
+++ modules/vim/spec/classes/vim_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe 'vim' do
+  let(:local_user) {'tmolnar'}
+
   it { is_expected.to compile.with_all_deps }
   it { is_expected.to contain_package('vim-package').with_ensure('present') }
-  it { is_expected.to contain_file('vim-config') }
+  it { is_expected.to contain_file('vim-config').with(
+    'ensure'  => 'file',
+    'path'    => '/home/tmolnar/.vimrc',
+    'source'  => 'puppet:///modules/vim/.vimrc',
+    'mode'    => '0755',
+    'owner'   => 'tmolnar',
+    'group'   => 'tmolnar',
+  ) }
   it { is_expected.to contain_package('vim-puppet-package').with_ensure('present') }
   it { is_expected.to contain_package('vim-addon-manager-package').with_ensure('present') }
   it { is_expected.to contain_exec('enable-vim-puppet') }
